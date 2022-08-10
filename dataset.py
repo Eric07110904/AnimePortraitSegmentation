9cb2df419f340b967d3868809119d39fdb66fd42
Eric07110904 <azsx26735546@gmail.com>
2022-08-10 16:08:52 +0800

    update dataset

--- dataset.py
+++ dataset.py
@@ -7,7 +7,8 @@ from torch.utils.data import Dataset
 from torchvision import transforms as T
 from PIL import Image
 import albumentations as A
-import torch 
+import torch, random 
+
 color_map = [
     np.array([0, 0, 0]),
     np.array([0, 0, 128]),
@@ -27,11 +28,25 @@ def segmentationMap2Label(mask_img: np.array, color_map: List) -> np.array:
         label[(mask_img==color).all(axis=2)] = i  
     return label 
 
+def showImageWithMask(image: np.array, mask: np.array) -> None: 
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(image)
+    ax2.imshow(mask)
+    plt.show()
+
+def save_augmented(image: np.array, mask: np.array, filename: str, s: int) -> None: 
+    aug_image = Image.fromarray(image.astype("uint8"), "RGB")
+    aug_image.save("./aug_image/"+filename+"_seed_"+str(s)+".jpg")
+    aug_mask = Image.fromarray(mask.astype("uint8"), "RGB")
+    aug_mask.save("./aug_mask/"+filename+"_seed_"+str(s)+".png")
+    print("[INFO] save augmented seed: ", s)
+
 class SegmentationDataset(Dataset):
-    def __init__(self, img_dir, mask_dir, transform=None) -> None:
+    def __init__(self, img_dir, mask_dir, s, transform=None) -> None:
         self.img_dir = img_dir 
         self.mask_dir = mask_dir
         self.filenames = [p.split("\\")[1][:-4] for p in glob.glob(self.mask_dir + "*.png")]
+        self.transforms = transform 
         self.class_map =  [
             np.array([0, 0, 0]),
             np.array([0, 0, 128]),
@@ -41,25 +56,45 @@ class SegmentationDataset(Dataset):
             np.array([128, 0, 128]),
             np.array([128, 128, 0])    
         ]
+        self.s = s
     def __len__(self):
         return len(self.filenames)
     
     def __getitem__(self, index):
         img = np.array(Image.open(self.img_dir + self.filenames[index]+".jpg").convert("RGB"))
         mask = np.array(Image.open(self.mask_dir + self.filenames[index]+".png").convert("RGB"))
-        resize_transform = A.Resize(512, 512, interpolation=cv2.INTER_NEAREST)
-        resized = resize_transform(image = img, mask=mask)
-        img, mask = resized["image"], resized["mask"]
+        if self.transforms is not None:  
+            random.seed(self.s)
+            aug = self.transforms(image=img, mask=mask)
+            img, mask = aug["image"], aug["mask"]
+        # save_augmented(img, mask, self.filenames[index], self.s)
+        # showImageWithMask(img, mask)
         mask = torch.from_numpy(segmentationMap2Label(mask, self.class_map)).long()
         t = T.Compose([T.ToTensor()])
         img = t(img)
         return img, mask # 3x512x512, 512x512
 
 if __name__ == "__main__":
-    d = SegmentationDataset("./data/train/", "./data/train_label/", None)
-    img, mask = d[0]
-    print(img.shape)
-    print(mask.shape)
-    plt.imshow(mask)
-    plt.show()
+    seed = [(i+1)*5 for i in range(15)]
+    augmented_transform = A.Compose([
+        #A.Resize(512, 512, interpolation=cv2.INTER_NEAREST), 
+        A.HorizontalFlip(), 
+        A.VerticalFlip(), 
+        A.GridDistortion(p=0.5),
+        A.RandomBrightnessContrast((0,0.2),(0,0.2)),
+        A.GaussNoise(),
+        A.Rotate(limit=(-30, 30), p=0.5)
+        ])
+    train_transform = A.Compose([
+        A.Resize(512, 512, interpolation=cv2.INTER_NEAREST), 
+        ]) 
+    for s in seed: 
+        print("[INFO] seed:", s)
+        d = SegmentationDataset("./data/train/", "./data/train_label/", s, augmented_transform)
+        for i in range(len(d)):
+            img, mask = d[i]
+            print(img.shape, mask.shape)
+            break 
+        break 
+
 
